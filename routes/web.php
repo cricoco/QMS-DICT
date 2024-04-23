519f6b83b8f73c076a27a1a82484b6d7e09518bc
John Doe <johndoe@email.com>
2024-04-23 23:54:40 +0800

    resources>views

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/library', function () {
+    return view('library');
+});
+
+Route::get('/manuals', function () {
+    return view('manuals');
+});
+
+Route::get('/forms', function () {
+    return view('forms');
 });
