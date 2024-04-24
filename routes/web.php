20f4d4b90d33800641ffa8bdea69b93e77775b8d
0xH04MI <200295@adzu.edu.ph>
2024-04-24 11:39:26 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+/* Requires user to be authenticated before accessing library */
+Route::get('/library', function () {
+    return view('library');                                 
+})->middleware(['auth', 'verified'])->name('library');
+
+// Route::get('/dashboard/manuals', function () {
+//     return view('manuals');                                 
+// })->middleware(['auth', 'verified'])->name('manuals');
+
+// Route::get('/dashboard/formats', function () {
+//     return view('formatz');                                 
+// })->middleware(['auth', 'verified'])->name('manuals');
+
+
+require __DIR__.'/auth.php';
