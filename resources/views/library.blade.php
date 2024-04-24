15de482edda2724fd97c2d87b17ccefbf490fb67
0xH04MI <200295@adzu.edu.ph>
2024-04-24 11:56:37 +0800

    with new four table under migrations

--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -1,1 +1,30 @@
-<h1>LIBRARY</h1>
+<li class="main-nav__item main-nav__community">
+                        <a class="subnav-toggle">
+                            <span class="arrow-down"></span>
+                        </a>
+                        <a href="/forum" class="main-nav__item-link nav__with-sub">Community</a>
+                        <ul class="main-nav__sub-menu hidden-for-medium-down">
+                            <li class="main-nav__sub-item">
+                                <a href="/forum" class="hide-mobile-nav main-nav__sub-item-link">Forum</a>
+                            </li>
+                            <li class="main-nav__sub-item">
+                                <a href="https://wiki.albiononline.com" class="hide-mobile-nav main-nav__sub-item-link">Wiki</a>
+                            </li>
+                            <li class="main-nav__sub-item">
+                                <a target="_blank" href="https://creators.albiononline.com" class="hide-mobile-nav main-nav__sub-item-link">
+                                    Creators Program
+                                </a>
+                            </li>
+                            <li class="main-nav__sub-item">
+                                    <a href="/twitch" class="hide-mobile-nav main-nav__sub-item-link">
+                                        Twitch Drops
+                                    </a>
+                                </li>
+                            <li class="main-nav__sub-item">
+                                <a href="/killboard" class="hide-mobile-nav main-nav__sub-item-link">Killboard</a>
+                            </li>
+                            <li class="main-nav__sub-item">
+                                <a href="/characterbuilder/" class="hide-mobile-nav main-nav__sub-item-link">Character Builder</a>
+                            </li>
+                        </ul>
+                    </li>
