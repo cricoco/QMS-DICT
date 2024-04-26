e59138d8b029e5e945535c04ba2e4a50a606e07c
0xH04MI <200295@adzu.edu.ph>
2024-04-26 08:05:27 +0800

    modified users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('type', ['admin', 'user'])->default('user');
+            $table->string('designation')->nullable();
             $table->timestamps();
         });
 
