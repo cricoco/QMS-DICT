360b2c4877cd4355eeff580b5ea11ff07a4ce648
Cedric <126557844+cricoco@users.noreply.github.com>
2024-04-29 07:32:05 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,41 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', function () {
+    // Auth check
+    if (!Auth::check()) {
+        // If not logged in, go login.
+        return redirect()->route('login');
+    } else {
+        return redirect()->route('home');
+    }
+});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/home', function () {
     return view('home');
-});
+})->middleware(['auth', 'verified'])->name('home');
+
+Route::get('/about-us', function () {
+    return view('about-us');
+})->middleware(['auth', 'verified'])->name('about-us');
 
 Route::get('/library', function () {
     return view('library');
-});
-
-Route::get('/manuals', function () {
+})->middleware(['auth', 'verified'])->name('library');
+Route::get('/library/manuals', function () {
     return view('manuals');
+})->middleware(['auth', 'verified'])->name('manuals');
+Route::get('/library/formats', function () {
+    return view('formats');
+})->middleware(['auth', 'verified'])->name('formats');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/forms', function () {
-    return view('forms');
-});
+/* Requires user to be authenticated before accessing library */
+
+require __DIR__.'/auth.php';
