c766ed36016e143875ddfa70f2ff297df640c60e
Cedric <126557844+cricoco@users.noreply.github.com>
2024-04-29 07:29:31 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,24 @@
-<h1> Home </h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/home-bg.css" rel="stylesheet" type="text/css">
+    <title>Home</title>
+</head>
+<body>
+    <div class="home-bg">
+        <ul>
+            <li><a href="{{ route('library') }}">Library</a></li>
+            <li><a href="news.asp">About Us</a></li>
+            <li><a href="{{ route('about-us') }}" style="margin-right: 950px;">About Us</a></li>
+
+            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        </ul>
+    </div>
+</body>
+</html>
