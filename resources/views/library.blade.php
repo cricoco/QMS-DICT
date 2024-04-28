6a93aad80de6868a324d3f4994e42c6cd6f95828
Cedric <126557844+cricoco@users.noreply.github.com>
2024-04-29 07:33:17 +0800

    Update library.blade.php

--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -11,7 +11,6 @@
         <ul>
             <li><a href="{{ route('home') }}" style="margin-right: 500px;">Home</a></li>
             <li><a href="{{ route('manuals') }}">Manuals</a></li>
-            <li><a href="{{ route('formats') }}">Formats</a></li>
             <li><a href="{{ route('formats') }}" style="margin-right: 400px;">Formats</a></li>
             <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
 
