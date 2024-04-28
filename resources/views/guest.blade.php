6727cc9dc0d12ddb46c26da365dabe45cdf78eb6
Cedric <126557844+cricoco@users.noreply.github.com>
2024-04-29 07:33:48 +0800

    Update guest.blade.php

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -5,8 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
         <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
         <title>DICT QMS Portal - Login</title>
         <!-- Fonts -->
@@ -18,9 +16,7 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
+               
                 <img src="https://upload.wikimedia.org/wikipedia/commons/a/aa/DICT-Logo-icon_only.png" style="width: 200px; height: 200px; ">
             </div>
 
