aa00e1308c4444328f0c03f2d075b5c177ce73a3
Cedric <126557844+cricoco@users.noreply.github.com>
2024-04-29 11:40:23 +0800

    Update UploadDataController.php

--- app/Http/Controllers/UploadDataController.php
+++ app/Http/Controllers/UploadDataController.php
@@ -4,10 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Personal;
+
 class UploadDataController extends Controller
   {
     public function index()
     {
       return view('uploadpage');
     }
+
+    public function store(Request $request)
+    {
+        $data=new personal;
+
+         if($request->file('file'))
+
+             {
+                 $file=request->file('file');
+                 $filename=time().'.'.$file->getClientOriginalExtension();
+                 $request->file->move('storage',$filename);
+
+                 $data->file=$filename;
+             }
+
+             $data->name=$request->name;
+             $data->email=$request->email;
+
+             $data->save();
+
+             return redirect->back();
+    }
+          
   }
