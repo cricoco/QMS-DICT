f4dee425ac265d1a6525848c60b95170be495ec6
Cedric <126557844+cricoco@users.noreply.github.com>
2024-04-29 09:11:01 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
 Route::get('/', function () {
     // Auth check
     if (!Auth::check()) {
@@ -10,9 +16,12 @@ Route::get('/', function () {
         return redirect()->route('home');
     }
 });
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::get('/home', function () {
     return view('home');
 })->middleware(['auth', 'verified'])->name('home');
@@ -24,12 +33,15 @@ Route::get('/about-us', function () {
 Route::get('/library', function () {
     return view('library');
 })->middleware(['auth', 'verified'])->name('library');
+
 Route::get('/library/manuals', function () {
     return view('manuals');
 })->middleware(['auth', 'verified'])->name('manuals');
+
 Route::get('/library/formats', function () {
     return view('formats');
 })->middleware(['auth', 'verified'])->name('formats');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
