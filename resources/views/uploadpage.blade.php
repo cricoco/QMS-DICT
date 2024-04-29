97c3f2427ec064166e8ae7db2345a25c01b16243
Cedric <126557844+cricoco@users.noreply.github.com>
2024-04-29 11:22:59 +0800

    Create uploadpage.blade.php

---
+++ resources/views/uploadpage.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+
+        <form>
+
+          <div>
+            <label>name</label>
+            <input type="text" name="name" placeholder="name">
+          </div>
+
+          <div>
+            <label>Email</label>
+            <input type="email" name="email" plaveholder="Email">
+          </div>
+
+          <div>
+            <label>Photo</label>
+            <input type="file" name="file">
+          </div>
+
+          <input type="submit" name="submit">
+        </form>
+</body>
+</html>
+
