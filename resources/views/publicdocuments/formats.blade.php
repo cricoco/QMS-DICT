6888872f05cae41429a76fcaf8499d255d201bfa
0xH04MI <200295@adzu.edu.ph>
2024-05-16 14:58:47 +0800

    added pub view for pub manuals and formats

--- resources/views/publicdocuments/formats.blade.php
+++ resources/views/publicdocuments/formats.blade.php
@@ -44,7 +44,7 @@
                                             <td>{{ $item->process_owner }}</td>
                                             <td>{{ $item->status }}</td>
                                             <td style="white-space: nowrap;">
-                                                <a href="{{ url('/p/document/' . $item->id) }}" title="View Document" class="btn btn-info btn-sm" style="background-color: #a881af; border-color: #a881af;"><i class="fa fa-eye" aria-hidden="true"></i></a>
+                                            <a href="javascript:void(0)" id="pubshow-documents" data-url="{{ route('publicdocuments.show', $item->id) }}" title="View Document" class="btn btn-info btn-sm" style="background-color: #a881af; border-color: #a881af;"><i class="fa fa-eye" aria-hidden="true"></i></a> <!-- View -->
 
                                                 <a href="{{ route('document.download', $item->file) }}" title="Download Document" class="btn btn-info btn-sm" style="background-color: #ffd450; border-color: #ffd450;" onclick="return confirm('This document is a protected copy. Click ok to download.');">
                                                 <i class="fa fa-download" aria-hidden="true"></i>
@@ -68,5 +68,45 @@
     </div>
 </div>
 <br>
+@include('publicdocuments.modal-view')
+@endsection
 
+@section('pubscript')
+<script type="text/javascript">
+    $(document).ready(function() {
+
+        
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $('body').on('click', '#pubshow-documents', function() {
+            var docURL = $(this).data('url');
+            $.get(docURL, function(data) {
+                $('#pubdocShowModal').modal('show');
+                $('#pub-doc-ref-code').text(data.doc_ref_code);
+                $('#pub-doc-title').text(data.doc_title);
+                $('#pub-status').text(data.status);
+                $('#pub-document-iframe').attr('src', "{{ asset('storage/documents/') }}/" + data.file);
+                $('#pub-division').text(data.division);
+                // $('#dmt-incharged').text(data.dmt_incharged);
+                $('#pub-process-owner').text(data.process_owner);
+                $('#pub-doc-type').text(data.doc_type);
+                $('#pub-req-reason').text(data.request_reason);
+                $('#pub-req-type').text(data.request_type);
+                $('#pub-requester').text(data.requester);
+                $('#pub-req-date').text(data.request_date);
+                $('#pub-rev-num').text(data.revision_num);
+                $('#pub-effic-date').text(data.efficitivity_date);
+                $('#pub-filename').text(data.file);
+                $('#pub-created-at').text(data.created_at);
+            })
+        });
+
+
+    });
+</script>
 @endsection
