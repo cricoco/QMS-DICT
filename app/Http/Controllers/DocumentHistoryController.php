74cb76c8bb720cb911aebdb5c34f4df9b3f09fcf
0xH04MI <200295@adzu.edu.ph>
2024-05-20 13:57:16 +0800

    fixed history bug

--- app/Http/Controllers/DocumentHistoryController.php
+++ app/Http/Controllers/DocumentHistoryController.php
@@ -26,11 +26,19 @@ class DocumentHistoryController extends Controller
 
     public function specifichistory($id)
     {
-        
-        $history = DocumentHistory::where('document_id', $id)
-                ->orderBy('updated_at', 'desc')
-                ->paginate(10);
-        
+        // Get the doc_ref_code of the document with the given id
+        $doc_ref_code = Document::where('id', $id)->value('doc_ref_code');
+
+        // Search for documents with the same doc_ref_code but different IDs
+        $related_documents = Document::where('doc_ref_code', $doc_ref_code)
+            ->where('id', '!=', $id)
+            ->pluck('id');
+
+        // get history for the related documents along with the current document
+        $history = DocumentHistory::whereIn('document_id', $related_documents->push($id)->toArray())
+            ->orderBy('updated_at', 'desc')
+            ->paginate(10);
+
         return view('specifichistory')->with('history', $history);
     }
 }
