ad4790dbaa355a2d738915ff0c4c09b16041e966
0xH04MI <200295@adzu.edu.ph>
2024-05-31 15:05:12 +0800

    fixed home and pub home tabs suggested by Sir Almark. Also added user manual button

--- resources/views/publicdocuments/layout.blade.php
+++ resources/views/publicdocuments/layout.blade.php
@@ -108,7 +108,9 @@
           </li>
         </ul>
         <ul class="navbar-nav ml-auto">
-
+          <li class="nav-item">
+            <a class="nav-link" href="{{ url('user_manual/DICT-QMS-User-Manual.pdf') }}" target="_blank" role="button" title="User Manual"><i class="fas fa-book" style="font-size:24px"></i></a>
+          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Welcome, {{ Auth::user()->name }}</a>
             <ul class="dropdown-menu">
@@ -218,4 +220,5 @@
   </script>
 </body>
 <!-- Created by AdZU OJT Group April 15, 2024 to May 31, 2024 Batch -->
+
 </html>
